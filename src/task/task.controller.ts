91c82767a7b5be5792db6f84a684d23f045189b3
andreikostsen <pks@tut.by>
2025-10-13 16:56:42 +0300

    task controller added

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TaskService } from './task.service';
 
 @Controller('task')
@@ -6,9 +6,12 @@ export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Get('all')
-
   findAll() {
     return this.taskService.findAll();
   }
 
+  @Get('by-id/:id')
+  findOne(@Param('id') id: number) {
+    return this.taskService.findById(id);
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,20 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class TaskService {
+  private tasks = [
+    {
+      id: 1,
+      title: 'Nest JS',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'JS',
+      isCompleted: true
+    }
+  ]
+
   findAll() {
-    return [
-      {
-        id: 1,
-        title: 'Nest JS',
-        isCompleted: false
-      },
-      {
-        id: 2,
-        title: 'JS',
-        isCompleted: true
-      }
-    ]
+    return this.tasks
+  }
+
+  findById(id: number) {
+    const task = this.tasks.find((task) => task.id == id);
+
+    if (!task) {
+      throw new NotFoundException('No task with id ' + id);
+    }
+
+    return task;
   }
 
 }
